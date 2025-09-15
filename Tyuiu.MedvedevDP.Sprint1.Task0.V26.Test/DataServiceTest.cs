f094605c80bf2a8a8479da05e4bcd968036be0f9
danii <danii@DANIIL>
2025-09-15 20:08:30 +0500

    Add project files.

---
+++ Tyuiu.MedvedevDP.Sprint1.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MedvedevDP.Sprint1.Task0.V26.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint1.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint1.Task0.V26/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MedvedevDP.Sprint1.Task0.V26.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
