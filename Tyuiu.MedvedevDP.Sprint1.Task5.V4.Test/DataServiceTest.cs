1a560a48c76d70e26fee372c9e60bef003ef8a47
danii <danii@DANIIL>
2025-09-22 14:10:44 +0500

    task5

---
+++ Tyuiu.MedvedevDP.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MedvedevDP.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.MedvedevDP.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService dataService = new DataService();
+
+            int x = 13257;
+            int res = dataService.SecondsToHours(x);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 3;
+
+            Assert.AreEqual(wait,result);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MedvedevDP.Sprint1.Task5.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+        int x;
+
+        x=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(dataService.SecondsToHours(x));
+    }
+}
